8f477028d0f19b373c30756ebde100aca2a0728e
戴先森 <572150340@qq.com>
2020-11-03 13:11:21 +0800

    react组件状态提升

--- src/viewModules/Calculator.jsx
+++ src/viewModules/Calculator.jsx
@@ -1,20 +1,56 @@
 import React from 'react';
 import TemperatureInput from './TemperatureInput.jsx'
 
-// function BoilingVerdict(props) {
-//     if (props.celsius >= 100) {
-//         return <p>The water would boil.</p>;
-//     }
-//     return <p>The water would not boil.</p>;
-// }
+// 现在的需求是希望输入一个输入框 另外一个也跟着变动 学习react的状态提升
+
+// 定义摄氏度与华氏度之间相互转换的函数
+
+function toCelsius(fahrenheit) {
+	return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+  	return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
 
 class Calculator extends React.Component {
+	constructor(props) {
+		super(props)
+		this.handleCelsiusChange = this.handleCelsiusChange.bind(this)
+		this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this)
+		this.state = {temperature: '', scale: 'c'}
+	}
+	handleCelsiusChange(temperature) { // 更改类型为c输入框的值
+		this.setState({
+			temperature: temperature,
+			scale: 'c'
+		})
+	}
+	handleFahrenheitChange(temperature) { // 更改类型为f输入框的值
+		this.setState({
+			temperature: temperature,
+			scale: 'f'
+		})
+	}
     render() {
+    	const scale = this.state.scale
+    	const temperature = this.state.temperature
+    	const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    	const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
         return (
-            
             <div className="calculatorBox">
-                <TemperatureInput scale="c" />
-                <TemperatureInput scale="f" />
+                <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={this.handleCelsiusChange} />
+                <TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={this.handleFahrenheitChange} />
             </div>
         );
     }
--- src/viewModules/TemperatureInput.jsx
+++ src/viewModules/TemperatureInput.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import './TemperatureInput.scss'
 
 // 定义不同输入框的字段
-const scaleName = {
-    c: 'Celsius',
-    f: 'Fahrenheit'
-}
+// const scaleName = {
+//     c: 'Celsius',
+//     f: 'Fahrenheit'
+// }
+
+// 当两个组件之间的值需要联动时，需要用到react的状态提升。这个时候应该使用props的值，而不应该使用state的值
 
 class TemperatureInput extends React.Component {
     constructor(props) {
@@ -13,17 +16,18 @@ class TemperatureInput extends React.Component {
         this.state = {temperature: ''};
     }
     handleChange(e) {
-        this.setState({temperature: e.target.value});
+        // this.setState({temperature: e.target.value});
+        this.props.onTemperatureChange(e.target.value)
     }
     render() {
-        const temperature = this.state.temperature
+        const temperature = this.props.temperature // 使用props的的值
         const scale = this.props.scale
         return (
-            <fieldset>
-            <legend>Enter temperature in {scaleName[scale]}:</legend>
-            <input
-                value={temperature}
-                onChange={this.handleChange} />
+            <fieldset className="fieldsetBox">
+                <legend>Enter temperature in {scale}:</legend>
+                <input
+                    value={temperature}
+                    onChange={this.handleChange} />
             </fieldset>
         )
     }
